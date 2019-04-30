b84f71db6f2fbd6b17e6cfb471e7c0585788643d
Kobi Ederi <kederi@msn.com>
2019-04-30 13:34:35 -0500

    Updated SliderPanel

--- src/GUI/SliderPanel.java
+++ src/GUI/SliderPanel.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import java.awt.event.MouseListener;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -10,17 +12,61 @@ import javax.swing.JTextField;
 public class SliderPanel extends JPanel
 {
 	private JLabel hamming = new JLabel("Enter Hamming Dist:");
-	private JSlider hammNum = new JSlider(0, 4);
+	private JSlider hammNum = new JSlider(1, 4);
 	private JTextField num;
+	
+	int val;
 	public SliderPanel()
 	{
 		hammNum.setSize(200, 50);
-		num = new JTextField(hammNum.getValue());
+		hammNum.setMajorTickSpacing(10);
+		hammNum.setMinorTickSpacing(1);
+		hammNum.setPaintTicks(true);
+		hammNum.setPaintLabels(true);
+		
+		val = hammNum.getValue();
+		num = new JTextField( val + "");
+	
 		
 		//Add the components to the panel
+		add(hamming);
 		add(num);
 		add(hammNum);
-		add(hamming);
 	}
+	/**
+	public void getCurrentSlide()
+	{
+		int val;
+		while(hammNum.getValueIsAdjusting())
+		{
+			if(hammNum.getValue() == 1)
+			{
+				val = 1;
+				num.setText(val + "");
+			}
+			else if (hammNum.getValue() == 2)
+			{
+				val = 2;
+				num.setText(val + "");
+			}
+			else if (hammNum.getValue() == 3)
+			{
+				val = 3;
+				num.setText(val + "");
+			}
+			else if (hammNum.getValue() == 4)
+			{
+				val = 4;
+				num.setText(val + "");
+			}
+			else
+			{
+				val = 0;
+				num.setText(val + "");
+			}
+		}
+		
+	}
+	**/
 	
 }
